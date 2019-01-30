7e928b32eed57b2040be226f7c2172e085d13859
CKC\extern.koutsodimakis <extern.Giorgos.Koutsodimakis@ckc.de>
2019-01-30 16:53:11 +0100

    component state navigation

--- src/Components/Photowall.js
+++ src/Components/Photowall.js
@@ -1,15 +1,24 @@
-import React,{Component} from 'react'
+import React from 'react'
 import Photo from './Photo'
+import PropTypes from 'prop-types'
+import AddPhoto from './AddPhoto'
 
-class Photowall extends Component{
-    render(){
-        return(
-        <div className="photoGrid">
-                    {this.props.posts.map((post, index)=><Photo key={index} post={post}/>)}
+function Photowall(props) {
+    return (
+        <div> 
+           <button className="addIcon"> </button>
+            <div className="photoGrid">
+                {props.posts.map((post, index) => <Photo key={index} post={post} onRemovePhoto={props.onRemovePhoto} />)}
+            </div>
         </div>
+
     )
-    }
 }
 
+Photowall.propTypes = {
+
+    posts: PropTypes.array.isRequired,
+    onRemovePhoto: PropTypes.func.isRequired
+}
 
 export default Photowall
